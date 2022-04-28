0a4e0c2e80b8498b02acc42536bc975d8b29f2cb
stephencarmody <Stephen.Carmody@wolterskluwer.com>
2022-04-29 06:38:05 +1000

    asfd

--- setup_tmux.sh
+++ setup_tmux.sh
@@ -1,61 +1,61 @@
 #!/usr/bin/env bash
 
-missing_packages=()
-if ! command -v aclocal &> /dev/null; then
-	missing_packages+=("autotools-dev")
-	missing_packages+=("automake")
-fi
-if ! command -v libtoolize &> /dev/null; then missing_packages+=("libtool"); fi
 
-if [ ${#missing_packages[@]} -gt 0 ]; then
-	missing_packages=$(IFS=" " ; echo "${missing_packages[*]}")
-	echo "missing packages... sudo apt install ${missing_packages}"
-	exit
+if command -v tmux &> /dev/null; then
+tmux_required="tmux 3.4-rc"
+tmux_version=$(tmuxz -V 2> /dev/null | head -1)
+highest_version=$(echo -e "${tmux_required}\n${tmux_version}" | sort -V | head -1)
+echo "${highest_version}"
+if [ "${highest_version}" != "${tmux_required}" ]; then
+  echo "${tmux_required} needs installing"
+else
+	echo "${tmux_required} or newer available"
 fi
 
 exit
 
+if [ "${EUID}" -ne 0 ]; then
+	echo "run as root; i.e. sudo ./setup_tmux.sh"
+	exit
+fi
+
+required=(libncurses5-dev libevent-dev)
+missing=()
+for package in "${required[@]}"; do
+	checking="checking for ${package}... "
+	if dpkg-query -l "$package" &> /dev/null; then
+		checking="${checking}yes"
+	else
+		checking="${checking}no"
+		missing+=("${package}")
+	fi
+	echo ${checking}
+done
+
 set -e
 
-cd ${HOME}
+if [ ${#missing[@]} -gt 0 ]; then
+	echo "installing ${#missing[@]} packages..."
+fi
 
-# Clean up
-rm -rf ncurses-6.3*
-rm -rf libevent-2.1*
-rm -rf tmux-3.3-rc*
+for package in "${missing[@]}"; do
+	apt --yes install "${package}"
+done
 
-# Install ncurses
-wget "https://invisible-mirror.net/archives/ncurses/ncurses-6.3.tar.gz"
-tar xvf "ncurses-6.3.tar.gz"
-cd "ncurses-6.3"
-./configure --prefix=${HOME}/.local
-make
-make install
-cd ..
 
-# Install libevent
-wget "https://github.com/libevent/libevent/releases/download/release-2.1.12-stable/libevent-2.1.12-stable.tar.gz"
-tar xvf "libevent-2.1.12-stable.tar.gz"
-cd "libevent-2.1.12-stable"
-./autogen.sh
-./configure --prefix=${HOME}/.local
-make
-make install
-cd ..
+rm -rf tmp
+mkdir tmp
+cd tmp
 
 # Install tmux
 wget "https://github.com/tmux/tmux/releases/download/3.3-rc/tmux-3.3-rc.tar.gz"
 tar xvf "tmux-3.3-rc.tar.gz"
 cd "tmux-3.3-rc"
-./configure --prefix=${HOME}/.local \
-    CFLAGS="-I${HOME}/.local/include -I${HOME}/.local/include/ncurses" \
-    LDFLAGS="-L${HOME}/.local/include -L${HOME}/.local/include/ncurses -L${HOME}/.local/lib"
+./configure
 make
 make install
 cd ..
 
 
-# Clean up
-rm -rf ncurses-6.3*
-rm -rf libevent-2.1*
-rm -rf tmux-3.3-rc*
+cd ..
+rm -rf tmp
