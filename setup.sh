fefb2abca80d2fee67d7bb628b971aabe8175850
sunladen <sunladenmail@gmail.com>
2022-04-29 14:12:46 +1000

    asdf

--- setup.sh
+++ setup.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
+# global git config
+git config --global user.email "sunladenmail@gmail.com"
+git config --global user.name "sunladen"
+git config --global credential.helper "/mnt/c/Program\ Files/Git/mingw64/libexec/git-core/git-credential-manager-core.exe"
+
+# disable login banner
 touch ~/.hushlogin
 
-rm -f ~/.profile
-
+# make sure there is a $HOME/.config directory
 mkdir -p ~/.config
 
+# symlink .dotfiles
 rm -rf ~/.bash_aliases && ln -s $PWD/bash/.bash_aliases ~/.bash_aliases
 rm -rf ~/.bash_profile && ln -s $PWD/bash/.bash_profile ~/.bash_profile
 rm -rf ~/.bashrc && ln -s $PWD/bash/.bashrc ~/.bashrc
@@ -15,8 +21,10 @@ rm -rf ~/.config/nvim && ln -s $PWD/.config/nvim ~/.config/nvim
 rm -rf ~/.tmux.conf && ln -s $PWD/.tmux.conf ~/.tmux.conf
 rm -rf ~/.config/ranger && ln -s $PWD/.config/ranger ~/.config/ranger
 
+# source new symlinked $HOME/.bashrc
 . ~/.bashrc
 
+# question whether to go on and perform package management and tool installs
 read -r -p "run package purge, install and updates (requires sudo)? [Yn] " response
 case "$response" in
 	[Yy])
